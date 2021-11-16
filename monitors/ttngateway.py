11882105444390605a83c7c811a3a8e4f4fe6ad3
rv355 <rv355@cam.ac.uk>
2021-11-16 00:38:54 +0000

    error check

--- monitors/ttngateway.py
+++ monitors/ttngateway.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from json.decoder import JSONDecodeError
 import requests
 from time import sleep, time
@@ -55,107 +56,100 @@ class TTNGateway:
             disconnected, connected = 0, 0
 
             try:
-                if all_gateway_response.json()['gateways']:
-                    pass
-            except JSONDecodeError:
-                continue
 
-            for gateway in all_gateway_response.json()['gateways']:
-                gateway_id = gateway['ids']['gateway_id']
-                status_response = requests.get(self.settings['gateway_status_url'].format(gateway_id), headers= self.headers)
+                for gateway in all_gateway_response.json()['gateways']:
+                    gateway_id = gateway['ids']['gateway_id']
+                    status_response = requests.get(self.settings['gateway_status_url'].format(gateway_id), headers= self.headers)
+                    
+                    if 'code' in list(status_response.json().keys()):
+                        disconnected+=1
 
-                try:
-                    if list(status_response.json().keys()):
-                        pass
-                except JSONDecodeError:
-                    continue
-                
-                if 'code' in list(status_response.json().keys()):
-                    disconnected+=1
-
-                    if self.gateways[gateway_id]['status'] == 'connected':
-                        event['down'].append(gateway_id)
-                        event_flag = True
-
-                    gateway_status['gateways'].update(
-                            {
-                                gateway_id: {
-                                    'gateway_id': gateway_id, 
-                                    'gateway_status': 'disconnected',
-                                    'gateway_status_message': ''}
-                            }
-                        )
-                    self.gateways[gateway_id]['status'] = 'disconnected'
-                else:
-                    last_mesage_time = get_time_since_epoch(status_response.json()['last_uplink_received_at'])
-                    if time() - last_mesage_time < self.settings['gateway_update_threshold']:
-                        if self.gateways[gateway_id]['status'] == 'disconnected':
-                            event['up'].append(gateway_id)
-                            event_flag = True
-                        connected+=1
-                        gateway_status['gateways'].update(
-                                {
-                                    gateway_id: {
-                                        'gateway_id': gateway_id, 
-                                        'gateway_status': 'connected',
-                                        'gateway_status_message': last_mesage_time
-                                    }
-                                }
-                            )
-                        self.gateways[gateway_id] = {'status': 'connected'}
-                    else:
                         if self.gateways[gateway_id]['status'] == 'connected':
                             event['down'].append(gateway_id)
                             event_flag = True
-                        self.gateways[gateway_id]['status'] = 'disconnected'
-                        disconnected+=1
+
                         gateway_status['gateways'].update(
                                 {
                                     gateway_id: {
                                         'gateway_id': gateway_id, 
                                         'gateway_status': 'disconnected',
-                                        'gateway_status_message': last_mesage_time
-                                    }
+                                        'gateway_status_message': ''}
                                 }
                             )
+                        self.gateways[gateway_id]['status'] = 'disconnected'
+                    else:
+                        last_mesage_time = get_time_since_epoch(status_response.json()['last_uplink_received_at'])
+                        if time() - last_mesage_time < self.settings['gateway_update_threshold']:
+                            if self.gateways[gateway_id]['status'] == 'disconnected':
+                                event['up'].append(gateway_id)
+                                event_flag = True
+                            connected+=1
+                            gateway_status['gateways'].update(
+                                    {
+                                        gateway_id: {
+                                            'gateway_id': gateway_id, 
+                                            'gateway_status': 'connected',
+                                            'gateway_status_message': last_mesage_time
+                                        }
+                                    }
+                                )
+                            self.gateways[gateway_id] = {'status': 'connected'}
+                        else:
+                            if self.gateways[gateway_id]['status'] == 'connected':
+                                event['down'].append(gateway_id)
+                                event_flag = True
+                            self.gateways[gateway_id]['status'] = 'disconnected'
+                            disconnected+=1
+                            gateway_status['gateways'].update(
+                                    {
+                                        gateway_id: {
+                                            'gateway_id': gateway_id, 
+                                            'gateway_status': 'disconnected',
+                                            'gateway_status_message': last_mesage_time
+                                        }
+                                    }
+                                )
 
-            if event_flag:
-                gateway_status['connected'] = connected
-                gateway_status['disconnected'] = disconnected
-                gateway_status['total'] = connected + disconnected
-                acp_event = {
-                    'up': {
-                        'acp_event_type': 'up',
-                        'acp_event_value': event['up']
-                    },
-                    'down': {
-                        'acp_event_type': 'down',
-                        'acp_event_value': event['down']
+                if event_flag:
+                    gateway_status['connected'] = connected
+                    gateway_status['disconnected'] = disconnected
+                    gateway_status['total'] = connected + disconnected
+                    acp_event = {
+                        'up': {
+                            'acp_event_type': 'up',
+                            'acp_event_value': event['up']
+                        },
+                        'down': {
+                            'acp_event_type': 'down',
+                            'acp_event_value': event['down']
+                        }
                     }
-                }
 
-                monitor_event_message = {
-                    'acp_ts': str(time()),
-                    'acp_id': self.settings['gateway_acp_id'],
-                    'acp_type': self.settings['acp_type_id'],
-                    'acp_event': acp_event,
-                    'payload_cooked': gateway_status
-                }
-                print('Event Message:', monitor_event_message)
-                self.client.publish(self.settings['gateway_topic'], json.dumps(monitor_event_message), qos=0)
-            elif timer%self.settings['update_interval'] == 0:
-                gateway_status['connected'] = connected
-                gateway_status['disconnected'] = disconnected
-                gateway_status['total'] = connected + disconnected
+                    monitor_event_message = {
+                        'acp_ts': str(time()),
+                        'acp_id': self.settings['gateway_acp_id'],
+                        'acp_type': self.settings['acp_type_id'],
+                        'acp_event': acp_event,
+                        'payload_cooked': gateway_status
+                    }
+                    print('Event Message:', monitor_event_message)
+                    self.client.publish(self.settings['gateway_topic'], json.dumps(monitor_event_message), qos=0)
+                elif timer%self.settings['update_interval'] == 0:
+                    gateway_status['connected'] = connected
+                    gateway_status['disconnected'] = disconnected
+                    gateway_status['total'] = connected + disconnected
 
-                monitor_event_message = {
-                    'acp_ts': str(time()),
-                    'acp_id': self.settings['gateway_acp_id'],
-                    'acp_type': self.settings['acp_type_id'],
-                    'payload_cooked': gateway_status
-                }
-                print('Periodic Message:', monitor_event_message)
-                self.client.publish(self.settings['gateway_topic'], json.dumps(monitor_event_message), qos=0)
+                    monitor_event_message = {
+                        'acp_ts': str(time()),
+                        'acp_id': self.settings['gateway_acp_id'],
+                        'acp_type': self.settings['acp_type_id'],
+                        'payload_cooked': gateway_status
+                    }
+                    print('Periodic Message:', monitor_event_message)
+                    self.client.publish(self.settings['gateway_topic'], json.dumps(monitor_event_message), qos=0)
 
-            sleep(self.settings['query_interval'])
-            timer+=self.settings['query_interval']
+                sleep(self.settings['query_interval'])
+                timer+=self.settings['query_interval']
+            except JSONDecodeError or KeyError:
+                print(sys.exc_info(), file=sys.stderr)
+                continue
